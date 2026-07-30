8f509a760790f8202ec662a76338948c57a2e31b
Kacper Lasota <56150911+kasperro3@users.noreply.github.com>
2026-07-30 20:55:55 +0200

    Gear selector voltage range update

--- App/Src/stalk.c
+++ App/Src/stalk.c
@@ -12,10 +12,10 @@ static float pinVoltageRange [PIN_STATES][2] = {
 };
 
 static float selectorVoltageRange [4][2] = {
-    {0, 0.55}, // D
-    {0.551, 1.66}, // N
-    {1.661, 2.76}, // R
-    {2.761, 3.3} // P
+    {0.1, 0.545}, // D
+    {0.545, 1.71}, // N
+    {1.71, 2.765}, // R
+    {2.765, 3.3} // P
 };
 
 typedef enum {
